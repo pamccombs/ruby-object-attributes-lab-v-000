626b95428fb713e53d9c7bc76934f1448f958451
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-13 01:47:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,23 @@
 class Dog
   
+  #name
+  
   def name=(name)
     @name = name
   end
   
   def name
     @name
+  end
+  
+  #breed
+  
+  def breed=(breed)
+    @breed = breed
+  end
+    
+  def breed
+    @breed
   end
   
 end
