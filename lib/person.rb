c808fcc9425b94de6d7f3ba5df146d04cf337239
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-13 01:50:56 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  
+  #name
+  
+  def name=(name)
+    @name = name
+  end
+  
+    def name
+      @name
+    end
+  
+  #job
+  
+  def job=(job)
+    @job = job
+  end
+    
+    def job
+      @job
+    end
+end
