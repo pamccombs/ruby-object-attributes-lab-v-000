3f86de9b6421d76e04c338cb209de3407131a0f6
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-13 01:50:22 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  
+  #name
+  
+  def name=(name)
+    @name = name
+  end
+  
+    def name
+      @name
+    end
+  
+  #job
+  
+  def job=(job)
+    @job = job
+    
+    def job
+      @job
+    end
+end
